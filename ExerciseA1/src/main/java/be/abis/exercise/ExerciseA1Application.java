1b73671ba396205fb164fcbdc96728fe2db31b96
krisbroodcoorens <kris.broodcoorens@bnpparibasfortis.com>
2020-10-14 15:25:21 +0200

    Exercise A1

---
+++ ExerciseA1/src/main/java/be/abis/exercise/ExerciseA1Application.java
@@ -0,0 +1,13 @@
+package be.abis.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseA1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseA1Application.class, args);
+	}
+
+}
