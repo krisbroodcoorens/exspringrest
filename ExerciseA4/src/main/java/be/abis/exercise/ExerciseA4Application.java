5b58e9ee94c8833b96437716e31dbb095782db93
krisbroodcoorens <kris.broodcoorens@bnpparibasfortis.com>
2020-10-15 13:01:43 +0200

    Exercise A4

---
+++ ExerciseA4/src/main/java/be/abis/exercise/ExerciseA4Application.java
@@ -0,0 +1,13 @@
+package be.abis.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseA4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseA4Application.class, args);
+	}
+
+}
